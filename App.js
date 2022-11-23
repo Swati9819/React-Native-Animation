69bef941d4139160aa67168121622a6411788ea0
Swati Pandey <swati.pandey@tothenew.com>
2022-11-23 17:12:03 +0530

    Intialize project

---
+++ App.js
@@ -0,0 +1,9 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+import AnimatedScreen from './src/AnimatedScreen'
+
+export default function App() {
+  return (
+    <AnimatedScreen />
+  )
+}
